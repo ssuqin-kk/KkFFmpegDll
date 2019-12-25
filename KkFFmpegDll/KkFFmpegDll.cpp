3d6d9f8f7dc0a4e80c80356e4ea41893c6d68978
ssuqin-kk <1986061846@qq.com>
2019-12-25 12:02:37 +0800

    first commit

---
+++ KkFFmpegDll/KkFFmpegDll.cpp
@@ -0,0 +1,31 @@
+﻿#include "KkFFmpegDll.h"
+
+DLLEXPORT bool registerFFmpeg() {
+	try {
+		FFMpegUtil::Register();
+	} catch (exception e) {
+		return false;
+	}
+	return true;
+}
+
+ DLLEXPORT bool callTranscode(bool exit[1],
+	const char* inputFilePath, const char* outputFilePath) {
+	TransCode transCode(exit[0]);
+	transCode.Initial(inputFilePath, outputFilePath);
+	int ret = transCode.TranscodeStep();
+	if (ret < 0) {
+		return false;
+	}
+	return true;
+}
+
+DLLEXPORT bool callCoverImage(const char* input, const char* output,
+	int width, int height) {
+	VideoCoverImage hikVideoCoverImage(input, output, width, height);
+	int ret = hikVideoCoverImage.CoverImageStep();
+	if (ret < 0) {
+		return false;
+	}
+	return true;
+}
---
+++ KkFFmpegDll/KkFFmpegDll.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "mp4convert/FFMpegUtil.h"
+#include "mp4convert/TransCode.hpp"
+#include "mp4convert/VideoCoverImage.hpp"
+
+#ifndef DLLEXPORT
+#define DLLEXPORT extern "C" __declspec(dllexport)
+#endif // !DLLEXPORT
+
+class KKFFmpegDll {
+public:
+	bool registerFFmpeg();
+
+	bool callTranscode(bool exit[1], const char* inputFilePath, const char* outputFilePath);
+
+	bool callCoverImage(const char* input, const char* output,
+		int width, int height);
+};
